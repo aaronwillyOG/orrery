5b7d215f054c2b0878e6aa49d706aa685144f8f2
SAIYAM-SHUKLA <115667516+SAIYAM-SHUKLA@users.noreply.github.com>
2024-10-07 01:05:44 +0530

    new commit

--- orrery-master/src/js/config.js
+++ orrery-master/src/js/config.js
@@ -1,19 +1,19 @@
 const planets = {
     //       [Name,         size,  dist sol,  orbit,  x coord, y coord, radius, sAngle, eAngle, colour,     textColour]
-    sun:     ["Sun",        15,    0,         0,      375,     375,     30,     0,      2,      "yellow",   "black"],
-    mercury: ["Mercury",    1,     2,         88,     335,     375,     5,      0,      2,      "silver",   "yellow"],
-    venus:   ["Venus",      2,     4,         225,    305,     375,     10,     0,      2,      "purple",   "yellow"],
-    earth:   ["Earth",      2.5,   6,         365,    275,     375,     12,     0,      2,      "green",    "white"],
-    mars:    ["Mars",       1.25,  8,         687,    245,     375,     10,     0,      2,      "red",      "white"],
-    jupiter: ["Jupiter",    27.5,  10,        4331,   205,     375,     20,     0,      2,      "orange",   "black"],
-    saturn:  ["Saturn",     24.75, 12,        10747,  155,     375,     18,     0,      2,      "moccasin", "black"],
-    uranus:  ["Uranus",     0,     0,         0,      105,     375,     16,     0,      2,      "aqua",     "black"],
-    neptune: ["Neptune",    0,     0,         0,      55,      375,     12,     0,      2,      "blue",     "white"]
+    sun:     ["Sun",        15,    0,         0,      775,     775,     30,     0,      2,      "yellow",    "black"],
+    mercury: ["Mercury",    1,     1.5,         88,     750,     750,     5,      0,      2,      "silver",    "white"],
+    venus:   ["Venus",      2,     3,         225,    740,     740,     10,     0,      2,      "purple",    "white"],
+    earth:   ["Earth",      2.5,   4.5,         365,    650,     650,     12,     0,      2,      "blue",    "white"],
+    mars:    ["Mars",       1.25,  6,         687,    600,     600,     10,     0,      2,      "red",      "white"],
+    jupiter: ["Jupiter",    27.5,  7.5,        4331,   550,     550,     20,     0,      2,      "orange",    "white"],
+    saturn:  ["Saturn",     24.75, 9,        10747,  500,     500,     18,     0,      2,      "moccasin",  "white"],
+    uranus:  ["Uranus",     2,     10.5,         20000,  450,     450,     16,     0,      2,      "aqua",     "white"],
+    neptune: ["Neptune",    3,     12,         40000,  400,     400,     12,     0,      2,      "blue",     "white"]
 };
 
 const properties = {
     canvasName: "orrery",
     canvasContext: "2d",
-    canvasWidth: 750,
-    canvasHeight: 750
+    canvasWidth: 1550,
+    canvasHeight: 1500
 };
